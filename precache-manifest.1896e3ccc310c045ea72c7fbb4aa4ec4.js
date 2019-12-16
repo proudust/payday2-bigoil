7591ccaec1b56008822bb3d4af1de3460b4b5f47
Proudust <proudust@gmail.com>
2019-12-16 15:05:04 +0900

    Updates

---
+++ precache-manifest.1896e3ccc310c045ea72c7fbb4aa4ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da78e79d6648252c2f372fa9a8eb09e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2dbad6f5bf8dd06c707",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d8568a15ff5383d8abf",
+    "url": "/static/js/2.1db4df67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1db4df67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2dbad6f5bf8dd06c707",
+    "url": "/static/js/main.d7314f78.chunk.js"
+  },
+  {
+    "revision": "6dec533339df80fac429",
+    "url": "/static/js/runtime-main.863a5c5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
