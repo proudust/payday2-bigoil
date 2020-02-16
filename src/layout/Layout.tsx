d795824858e3a2e262dc2f45c169bbe9f3d88c52
Proudust <proudust@gmail.com>
2020-02-16 15:08:32 +0900

    :sparkles: add support dark theme

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,18 +3,21 @@ import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { DefaultAppBar } from './AppBar';
+import { ApplyTheme } from './ApplyTheme';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div style={{ minWidth: 350 }}>
-    <CssBaseline />
-    <DefaultAppBar />
+  <ApplyTheme>
+    <div style={{ minWidth: 350 }}>
+      <CssBaseline />
+      <DefaultAppBar />
 
-    <Container component="main" maxWidth="sm" disableGutters>
-      {children}
-    </Container>
-  </div>
+      <Container component="main" maxWidth="sm" disableGutters>
+        {children}
+      </Container>
+    </div>
+  </ApplyTheme>
 );
